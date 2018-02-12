18b75708d24af840b49871ba1bc6b92df5eef0d6
Andres Velasquez <andres.velasquez@me.com>
2018-02-11 16:21:32 -0800

    added ability to add custom paths for moving files based on whatever show it is

--- rename_torrents/rename_torrents.py
+++ rename_torrents/rename_torrents.py
@@ -1,18 +1,18 @@
 import os, re
 
-files = os.listdir('/users/andres/Downloads/')
-showNames = ( # more shows can be added as long as it's 720p and the download site maintains the same naming conventions
-    'Boruto',
-    'One Piece'
-)
+files = os.listdir('/Volumes/Data/Torrents/')
+showNamesAndPaths = { # more shows can be added as long as it's 720p and the download site maintains the same naming conventions. Key is show name and value is path where it will be moved to after renaming
+    'Boruto': '/Volumes/Data/Videos/Anime/Boruto/', # make sure these paths begin and end with the '/' character
+    'One Piece': '/Volumes/Data/Videos/Anime/One Piece/TV/'
+}
 
 for fileName in files:
-    for show in showNames:
+    for show, path in showNamesAndPaths.iteritems():
         if show in fileName and '[720p]' in fileName:
-            fileExtension = os.path.splitext('/users/andres/Downloads/{}'.format(fileName))[1] # gets the extension of the file
-            if fileExtension != '.part':
+            fileExtension = os.path.splitext('/Volumes/Data/Torrents/{}'.format(fileName))[1] # gets the extension of the file
+            if fileExtension != '.part': # .part means torrent is not finished downloading so skip it
                 dropNumber = fileName.partition('[720p]')[0] # leave out 720 so episode number is the only number left
                 episodeNumber = re.findall('\d+', dropNumber)[0].zfill(3) # strip episode number out pad with zeros if necessary to 3 digit length
                 newFileName = '{} {}{}'.format(show, episodeNumber, fileExtension)
-                os.rename('/users/andres/Downloads/{}'.format(fileName), '/users/andres/Downloads/ztest/{}'.format(newFileName))
+                os.rename('/Volumes/Data/Torrents/{}'.format(fileName), '{}{}'.format(showNamesAndPaths[show], newFileName))
                 break # no need to check the rest of the show names, move on to the next file
