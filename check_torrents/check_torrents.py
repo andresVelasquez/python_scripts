43c8514f0fdd33c4bea0531f93e4cf9e534f7c7b
Andres Velasquez <andres.velasquez@me.com>
2019-08-12 11:51:10 -0700

    Add files via upload

--- check_torrents/check_torrents.py
+++ check_torrents/check_torrents.py
@@ -1,64 +1,73 @@
-import urllib2, cgi, pickle
+#!/usr/bin/python
+import urllib2, cgi, pickle, os, subprocess, logging
 from HTMLParser import HTMLParser
 
+if not os.path.isdir('/Volumes/Data/Torrents/'): # Quit program if volume 'Data' on external drive is not mounted
+    print('/Volumes/Data/Torrents/ is not available.')
+    quit()
+
 # GLOBAL VARIABLES
-with open('/users/admin/Documents/episodes.pkl') as openEpisode: # pickle file contains object that tracks what last episode downloaded for each show was
-    episodes = pickle.load(openEpisode)
 # websites won't accept a connection from 'python' so I got the user_agent headers for my browser from a website and use that in the request
 user_agent = 'USER_AGENT', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'
 headers = {'User-Agent': user_agent}
-episodeLinks = { # got these URLs from Network tab of Chrome's inspector under 'XHR'
-    'boruto': 'http://horriblesubs.info/lib/getshows.php?type=show&showid=869',
-    'onepiece': 'http://horriblesubs.info/lib/getshows.php?type=show&showid=347'
+shows = { # got these URLs from Network tab of Chrome's inspector under 'XHR'
+    'boruto': 'https://www.horriblesubs.info/api.php?method=getshows&type=show&showid=869',
+    'onepiece': 'https://www.horriblesubs.info/api.php?method=getshows&type=show&showid=347'
 }
-searchStrings = { # create search strings for each show using the episode number read from episodes.pkl
-    'boruto': 'Boruto - Naruto Next Generations - {} [720p]'.format(episodes['boruto']),
-    'onepiece': 'One Piece - {} [720p]'.format(episodes['onepiece'])
-}
+torrent_found = False
 
 class MyHTMLParser(HTMLParser): # I heard 3rd party library BeautifulSoup makes HTML parsing super easy but I wanted to try with built in library. Also used Python 2 code because I wanted it to work on my Mac server out of the box
-    def __init__(self):
+    def __init__(self, episode):
         HTMLParser.__init__(self)
-        self.show = ''
-        self.tdCount = 0
-        
+        self.current_episode = episode
+        self.high_episode = episode
+        self.found_ep = False
+
     def handle_starttag(self, tag, attrs):
-        if self.show and tag == 'td':
-            self.tdCount += 1
-        if self.tdCount == 2: # the torrent link is always in the second <td> tag after the searchString is found
-            try:
-                if attrs[0][1] == 'Torrent Link': # the tag with 'Torrent Link' will have the URL needed to download the torrent file
-                    torrentLink = attrs[1][1]
-                    request = urllib2.Request(url = torrentLink, data=None, headers = headers) # makes request to download torrent file
-                    torrentData = urllib2.urlopen(request)
-                    _, params = cgi.parse_header(torrentData.headers.get('Content-Disposition', '')) # get filename from the response headers
-                    filename = urllib2.unquote(params['filename']) # unquote will convert the URL filename to regular filename. For example, changing %20 to a space
-                    dataToWrite = torrentData.read()
-                    with open('/users/admin/Downloads/{}'.format(filename), 'wb') as saveTorrent: # save torrent file in downloads folder
-                        saveTorrent.write(dataToWrite)
-                        saveTorrent.close()
-                    episodes[self.show] += 1 # increment episode count for show corresponding to current searchString. This script won't work with the occasional special shows that have different name/number scheme
-                    with open('/users/admin/Documents/episodes.pkl', 'wb') as saveEpisode: # save pickle with current episode counts
-                        pickle.dump(episodes, saveEpisode)
-                        saveEpisode.close()          
-                    self.show = '' # reset perser properties so next searchString can be found
-                    self.tdCount = 0
-            except:
-                pass
+        if not self.found_ep and tag == 'div' and attrs[0][1] == 'rls-link link-720p' and int(attrs[1][1].split('-')[0]) > self.current_episode:
+            self.found_ep = True
+            if int(attrs[1][1].split('-')[0]) > self.high_episode:
+                self.high_episode = int(attrs[1][1].split('-')[0])
+        if self.found_ep and tag == 'a' and attrs[0][1] == 'Torrent Link':
+            torrentLink = attrs[1][1]
+            request = urllib2.Request(url = torrentLink, data=None, headers = headers) # makes request to download torrent file
+            torrentData = urllib2.urlopen(request)
+            _, params = cgi.parse_header(torrentData.headers.get('Content-Disposition', '')) # get filename from the response headers
+            filename = urllib2.unquote(params['filename']) # unquote will convert the URL filename to regular filename. For example, changing %20 to a space
+            dataToWrite = torrentData.read()
+            with open('/Users/Andres/Downloads/{}'.format(filename), 'wb') as saveTorrent: # save torrent file in Downloads folder
+                saveTorrent.write(dataToWrite)
+                saveTorrent.close()
+            self.found_ep = False
+            global torrent_found
+            torrent_found = True
 
-    def handle_data(self, data):
-        for show, searchString in searchStrings.iteritems():
-            if data == searchString:
-                self.show = show # searchString found so set self.show to name of show so I know which show's episode number to increment
-
-def makeParserData(): # takes the episodeLinks URLs and makes one concatenated HTML string to feed to the parser
-    parserData = ''
-    for link in episodeLinks.itervalues():
-        request = urllib2.Request(url = link, data=None, headers = headers)
-        response = urllib2.urlopen(request)
-        parserData += response.read()
+def makeParserData(url): # gets the shows URLs and makes one concatenated HTML string from the page to feed to the parser
+    request = urllib2.Request(url = url, data=None, headers = headers)
+    response = urllib2.urlopen(request)
+    parserData = response.read()
     return parserData
 
-showParser = MyHTMLParser()
-showParser.feed(makeParserData())
+with open('/Volumes/Data/Torrents/check_torrents/episodes.pkl') as openEpisode: # pickle file contains object that tracks what last episode downloaded for each show was
+    showList = pickle.load(openEpisode)
 
+for show, episode in showList.iteritems():
+    showParser = MyHTMLParser(episode)
+    try:
+        showParser.feed(makeParserData(shows[show]))
+        showList[show] = showParser.high_episode
+
+    except Exception as e:
+        logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S -', filename='/Volumes/Data/Torrents/check_torrents/error.log', level=logging.ERROR)
+        logging.error(str(e))
+        pass
+
+with open('/Volumes/Data/Torrents/check_torrents/episodes.pkl', 'wb') as saveEpisode: # save pickle with current episode counts
+    pickle.dump(showList, saveEpisode)
+    saveEpisode.close()
+
+# Open Transmission app if it's not running and torrent files were downloaded
+if torrent_found:
+    process = subprocess.Popen('pgrep Transmission', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if process.communicate()[0] == '':
+        subprocess.call(["/usr/bin/open", "-j", "-g", "-a", "/Applications/Transmission.app"])
